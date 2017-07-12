64e161759fbfccfb24def4c2510d785f728b6289
Ethan Lee <ylee157@jhu.edu>
2017-07-12 20:26:08 +0000

    Faith.py created online with Bitbucket

---
+++ Faith.py
@@ -0,0 +1,84 @@
+import discord
+import asyncio
+import convo
+import check
+import react
+
+convoFlow = (check.isMessage, [
+    (convo.tick, None),
+    (check.attn, [
+        (check.reply, react.reply),
+        (check.silence, react.silence),
+        (check.log, [
+            (check.closeLog, react.closeLog),
+            (check.potion, [
+                (check.noPot, react.noPot),
+                (check.potCount, react.potCount),
+                (check.potStats, react.potStats)
+            ]),
+            (check.rune, [
+                (check.noRune, react.noRune),
+                (check.runeCount, react.runeCount),
+                (check.runeStats, react.runeStats)
+            ]),
+            (check.refreshLog, react.refreshLog),
+            (check.newLog, react.newLog),
+            (check.pull, [
+                (check.pullDesc, react.pullDesc),
+                (check.pullNum, react.pullNum)
+            ]),
+            (check.participants, react.participants)
+        ]),
+        (check.suggestion, react.suggestion),
+        (check.tier, [
+            (check.wonPiece, react.wonPiece),
+            (check.undoWin, react.undoWin),
+            (check.deletePiece, react.deletePiece),
+            (check.tierCount, react.tierCount)
+        ]),
+        (check.newRaider, react.newRaider),
+        (check.gquit, react.gquit),
+        (check.introduce, react.introduce),
+        (check.creator, react.creator),
+        (check.function, react.function),
+        (check.define, react.define),
+        (check.statPri, react.statPri),
+        (check.relic, react.relic),
+        (check.neck, react.neck),
+        (check.missingEnch, react.missingEnch),
+        (check.tour, react.tour),
+        (check.musicBot, react.musicBot),
+        (check.disengage, react.disengage),
+        (check.greet, react.greet),
+        (check.mention, react.mention)
+    ], convo.refreshAttn),
+    (check.confuse, react.confuse)
+])
+
+async def trav(tree, message):
+    if tree[1] == None:
+        await tree[0](message)
+        return False
+    if tree[0](message):
+        if type(tree[1]) == list:
+            for subtree in tree[1]:
+                if await trav(subtree, message):
+                    if len(tree) == 3:
+                        await tree[2](message)
+                    return True
+        else:
+            await tree[1](message)
+            return True
+
+class Faith(discord.Client):
+
+    async def on_ready(self):
+        print("Yawn... Good morning?\n")
+        return
+
+    async def on_message(self, message):
+        await trav(convoFlow, message)
+
+faith = Faith()
+react.faith = faith
+faith.run("MzI4NjM2MDE5ODYxNjE4Njk5.DEWW9Q.XbgSdMAKk6SP81sRk6KYF1mA8pE")
