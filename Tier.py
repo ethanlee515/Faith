d2856bafab3e7f9cf31e7137da5d9d595243815c
Ethan Lee <ylee157@jhu.edu>
2017-07-13 21:13:28 +0000

    Tier features

--- Tier.py
+++ Tier.py
@@ -5,59 +5,41 @@ import json
 import pprint
 from misc import *
 
+currentRaid = None #REALLY should be None.
+#Setting to heroic as a hack for missing functionality.
+
 rec = eval(open("tierInfo.dat", "r").read())
+
+undoHistory = []
 
 async def save(fName = "tierInfo.dat"):
     async with aiofiles.open(fName, "w") as f:
         pp = pprint.PrettyPrinter(indent = 4)
-        await f.write(pp.pformat(rec) + "\n")
-
-# async def remakeRecord(rList = raiders, fName = "tierInfo.dat"):
-#     #This code just got 100% more idiot-proof
-#     async with aiofiles.open("oldInfo.dat", "w") as oldrec:
-#         pp = pprint.PrettyPrinter(indent = 4)
-#         oldrec.write(pp.pformat(tierInfo) + "\n")
-#     tierInfo.clear()
-#     for r in rList:
-#         print("searching up " + r + "...")
-#         try:
-#             tierInfo[r] = await Armory.getTierPieces(r)
-#         except RuntimeError:
-#             print(r + " not found.")
-#     await save(fName)
+        await f.write((pp.pformat(rec) + "\n"))
 
 nickname = {"sawbones": "asawbones",
             "sawbone": "asawbones",
             "bone": "asawbones",
             "bones": "asawbones",
             "rose": "rosaree",
-            "ezme": "ezmÃ«",
+            "ezme": "ezmë",
             "opd": "opdhealer",
             "op": "opdhealer",
             "deadeye": "deadeyedaisy",
-            "sharla": "sharlÃ¡",
+            "sharla": "sharlá",
             "danny": "yaois"}
 
-# async def updateRecord():
-#     for r in rList:
-#         print("searching up " + r + "...")
-#         try:
-#             tierInfo[r] = await Armory.getTierPieces(r)
-#         except RuntimeError:
-#             print(r + " not found.")
-#     await save(fName)
+defaultIL = {"LFR": 885, "normal": 900, "heroic": 915, "mythic": 930}
+
+def getIL(piece):
+    if piece == None:
+        return 0
+    return piece["IL"] if "IL" in piece else defaultIL[piece["from"]]
 
 def overWrite(p1, p2):
     if p2 == None:
         return True
-    dLvl = {"LFR": 1, "normal": 2, "heroic": 3, "mythic": 4}
-    if dLvl[p1["from"]] > dLvl[p2["from"]]:
-        return True
-    if "IL" not in p1:
-        return False
-    if ("IL" in p1 and "IL" not in p2) or p1["IL"] > p2["IL"]:
-        return True
-    return False
+    return getIL(p1) > getIL(p2)
 
 async def addPlayer(pName):
     r = pName.lower()
@@ -69,21 +51,60 @@ async def removePlayer(pName):
     del rec[r]
     await save()
 
-async def XWonY(x, slot, item):
-    x = nickname[x] if x in nickname else x
-    p2 = rec[x][slot]
-    if overWrite(item, p2):
-        rec[x][slot] = item
+def winIssues(name, slot, item):
+    if not overWrite(item, rec[name][slot]):
+        return "Already had " + pieceDesc(name, slot) + "."
+    if (getIL(rec[name][slot]) >= defaultIL["normal"]):
+        return "Replacing " + pieceDesc(name, slot) + "."
+    tierCount = 0
+    tfCount = 0
+    for s in Armory.slots:
+        if s != slot:
+            if getIL(rec[name][s]) >= defaultIL["normal"]:
+                tierCount += 1
+                if rec[name][s]["from"] == "LFR":
+                    tfCount += 1
+    if tierCount >= 4:
+        if tfCount == 0:
+            return "Already had %d tier pieces." % tierCount
+        else:
+            return ("Already had %d tier pieces, "
+                    "%d of which are TF'd from LFR."
+                        % (tierCount, tfCount))
+    return ""
 
-async def nameWon(name, difficulty, slot, il = None):
-    item = {'from': difficulty}
-    if il != None:
-        item["IL"] = il
-    XWonY(name, slot, item)
+async def award(names, slot, piece):
+    actions = []
+    for name in names:
+        if overWrite(piece, rec[name][slot]):
+            actions.append((name, slot, rec[name][slot], piece))
+            rec[name][slot] = piece
+    await save()
+    undoHistory.append(actions)
+
+async def forceOverwrite(name, slot, piece):
+    oldpiece = rec[name][slot]
+    undoHistory.append([(name, slot, oldpiece, piece)])
+    rec[name][slot] = piece
+    await save()
+    return pToS(oldpiece)
+
+async def undo():
+    if len(undoHistory) == 0:
+        return []
+    undone = []
+    lst = undoHistory.pop()
+    for (name, slot, oldpiece, newpiece) in lst:
+        rec[name][slot] = oldpiece
+        undone.append(name.capitalize() + "'s " + pToS(newpiece) + " " + slot +
+            " has been reverted back to " + pToS(oldpiece))
+    await save()
+    return undone
 
 def getName(nameIn):
     if nameIn == "":
         return None
+    nameIn = nameIn.lower()
     if nameIn in nickname:
         return nickname[nameIn]
     if nameIn in rec.keys():
@@ -110,8 +131,7 @@ def getSlot(m):
                 return k
     return None
 
-def pieceDesc(name, slot):
-    p = rec[name][slot]
+def pToS(p):
     if p == None:
         return "non-tier"
     info = p["from"]
@@ -119,13 +139,16 @@ def pieceDesc(name, slot):
         info += " %d" % p["IL"]
     return info
 
+def pieceDesc(name, slot):
+    return pToS(rec[name][slot])
+
 def extractPiece(m):
     m = m.lower()
     dLst = [("lfr", "LFR"),
         ("mythic", "mythic"),
         ("heroic", "heroic"),
         ("norm", "normal")]
-    diff = None
+    diff = currentRaid
     for (d0, d1) in dLst:
         if d0 in m:
             diff = d1
